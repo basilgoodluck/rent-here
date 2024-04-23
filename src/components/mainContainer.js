4c2371701796cec1cc85a2e56cd5353977dba46f
Basil Goodluck <basilgoodluck22@gmail.com>
2024-04-23 13:25:26 +0100

    Updated

---
+++ src/components/mainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContainer() {
+  return (
+    <div className='bg-black'>
+        <h1 className='text-3xl text-green-600'>Hello World</h1>      
+    </div>
+  )
+}
+
+export default MainContainer
